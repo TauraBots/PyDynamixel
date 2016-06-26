5cf679e03cfbc56b553b4aae11524a7be48eafa7
Rodrigo da Silva Guerra <tioguerra@gmail.com>
2016-06-26 16:19:49 -0400

    Added a test sync (wip)

---
+++ testSync.py
@@ -0,0 +1,31 @@
+import pydynamixel as dx
+from time import sleep
+from sys import exit
+
+#PORT = "/dev/ttyUSB0" # Serial port
+PORT = "/dev/ttyS5" # Serial port
+BAUDNUM = 8    # 222kbps baudrate
+TORQUE_ADDR = 0x18 # Address for torque enable
+GOALPOS_ADDR = 0x1E # Address for goal position
+
+# Initialize the socket
+socket = dx.dxl_initialize(PORT, BAUDNUM)
+if not socket:
+    exit("Failed to open socket")
+
+# Define the target servos
+servo_ids = [25,28,29,34,33]
+
+# Enable torque
+for servo_id in servo_ids:
+    dx.dxl_write_word(socket, servo_id, TORQUE_ADDR, 1)
+
+# Set goal positions
+for i in range(1023):
+    for servo_id in servo_ids:
+        dx.dxl_write_word(socket, servo_id, GOALPOS_ADDR, 4*i)
+    sleep(0.001)
+
+# Finish the connection with the socket
+dx.dxl_terminate(socket)
+
