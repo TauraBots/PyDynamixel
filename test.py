cd644e11450438adf78d3cf8b41397ca6fe6923a
Rodrigo da Silva Guerra <tioguerra@gmail.com>
2016-06-26 16:15:03 -0400

    Fixed the sample

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import pydynamixel as dx
 from time import sleep
 from sys import exit
 
-PORT = 4       # Port /dev/ttyS4
+#PORT = "/dev/ttyUSB0" # Serial port
+PORT = "/dev/ttyS5" # Serial port
 BAUDNUM = 8    # 222kbps baudrate
 TORQUE_ADDR = 0x18 # Address for torque enable
 GOALPOS_ADDR = 0x1E # Address for goal position
@@ -17,13 +18,13 @@ servo_ids = [25,28,29,34,33]
 
 # Enable torque
 for servo_id in servo_ids:
-    dx.dxl_read_word(socket, servo_id, TORQUE_ADDR, 1)
+    dx.dxl_write_word(socket, servo_id, TORQUE_ADDR, 1)
 
 # Set goal positions
-for i in range(4095):
+for i in range(1023):
     for servo_id in servo_ids:
-        dx.dxl_write_word(socket, servo_id, GOALPOS_ADDR, i)
-        sleep(0.05)
+        dx.dxl_write_word(socket, servo_id, GOALPOS_ADDR, 4*i)
+    sleep(0.001)
 
 # Finish the connection with the socket
 dx.dxl_terminate(socket)
