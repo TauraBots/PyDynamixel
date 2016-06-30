5ba4a40890654c83f7cd2a593b06c39e4339c868
Rodrigo da Silva Guerra <tioguerra@gmail.com>
2016-06-30 19:26:53 +0200

    Fixed the call to readAllCurrAngles

--- readJoints.py
+++ readJoints.py
@@ -11,17 +11,20 @@ lleg_port.attachJoints(lleg_joints)
 rleg_joints = [Joint(i) for i in [11,13,15,21,23,25,27]]
 rleg_port.attachJoints(rleg_joints)
 torso_joints = [Joint(i) for i in [31,32,33,34,35,36,41,42,51,52,53,61,62]]
-torso_servo_ids.attachJoints(torso_joints)
+torso_port.attachJoints(torso_joints)
 
 for i in range(1023):
+    lleg_port.receiveCurrAngles()
+    rleg_port.receiveCurrAngles()
+    torso_port.receiveCurrAngles()
     print 'lleg',
     for joint in lleg_joints:
-        print joint.servo_id,":",joint.currValue,
+        print str(joint.servo_id)+":"+str(joint.currValue),
     print 'rleg',
     for joint in rleg_joints:
-        print joint.servo_id,":",joint.currValue,
+        print str(joint.servo_id)+":"+str(joint.currValue),
     print 'trunk',
-    for joint in trunk_joints:
-        print joint.servo_id,":",joint.currValue,
+    for joint in torso_joints:
+        print str(joint.servo_id)+":"+str(joint.currValue),
     print
 
